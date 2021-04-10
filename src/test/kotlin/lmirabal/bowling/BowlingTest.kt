256f4dbf718a48a886f94571f9763aa16efbfb20
Luis Mirabal <lmirabal@gmail.com>
2021-04-10 11:52:03 +0100

    1 pin per throw
    Parse both throws and sum them.

--- src/test/kotlin/lmirabal/bowling/BowlingTest.kt
+++ src/test/kotlin/lmirabal/bowling/BowlingTest.kt
@@ -17,10 +17,21 @@ class BowlingTest {
 
         assertEquals(10, total)
     }
+
+    @Test
+    fun `knocks down 1 pin in each throw`() {
+        val total: Int = bowlingScore("11 11 11 11 11 11 11 11 11 11")
+
+        assertEquals(20, total)
+    }
 }
 
 private fun bowlingScore(input: String): Int {
     val frames = input.split(" ")
-        .map { frame -> if (frame.startsWith("-")) 0 else 1 }
+        .map { frame ->
+            val throw1 = if (frame.substring(0, 1) == "-") 0 else 1
+            val throw2 = if (frame.substring(1, 2) == "-") 0 else 1
+            throw1 + throw2
+        }
     return frames.sum()
 }
