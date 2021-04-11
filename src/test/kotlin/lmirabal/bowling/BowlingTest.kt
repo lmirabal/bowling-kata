18943fe0ae419dad1d7a77aec37b862cf3f45a17
Luis Mirabal <lmirabal@gmail.com>
2021-04-11 11:44:35 +0100

    Work out pin count of spare throw so the frame sum is 10
    The new test case knocks down pins in the first throw, so 10 can't be
    assumed for the spare throw but for the whole frame.
    
    This makes the parsing trickier as when a spare is seen, the value of
    the first throw is needed. However, as the first throw can't be a spare,
    split the parsing into 2, so the one for the second throw can have the
    first throw as param.

--- src/test/kotlin/lmirabal/bowling/BowlingTest.kt
+++ src/test/kotlin/lmirabal/bowling/BowlingTest.kt
@@ -6,57 +6,72 @@ import kotlin.test.assertEquals
 class BowlingTest {
     @Test
     fun `all misses`() {
-        val total: Int = bowlingScore("-- -- -- -- -- -- -- -- -- --")
+        val total = bowlingScore("-- -- -- -- -- -- -- -- -- --")
 
         assertEquals(0, total)
     }
 
     @Test
     fun `knocks down 1 pin in each frame`() {
-        val total: Int = bowlingScore("1- 1- 1- 1- 1- 1- 1- 1- 1- 1-")
+        val total = bowlingScore("1- 1- 1- 1- 1- 1- 1- 1- 1- 1-")
 
         assertEquals(10, total)
     }
 
     @Test
     fun `knocks down 1 pin in each throw`() {
-        val total: Int = bowlingScore("11 11 11 11 11 11 11 11 11 11")
+        val total = bowlingScore("11 11 11 11 11 11 11 11 11 11")
 
         assertEquals(20, total)
     }
 
     @Test
     fun `knocks down an extra pin with each throw`() {
-        val total: Int = bowlingScore("-- 1- 2- 3- 4- 5- 6- 7- 8- 9-")
+        val total = bowlingScore("-- 1- 2- 3- 4- 5- 6- 7- 8- 9-")
 
         assertEquals(45, total)
     }
 
     @Test
     fun `spares in first frame and knocks down 5 in the others`() {
-        val total: Int = bowlingScore("-/ 5- 5- 5- 5- 5- 5- 5- 5- 5-")
+        val total = bowlingScore("-/ 5- 5- 5- 5- 5- 5- 5- 5- 5-")
 
         assertEquals(60, total)
+    }
+
+    @Test
+    fun `knocks down 5 pins and spares except last frame`() {
+        val total = bowlingScore("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ --")
+
+        assertEquals(130, total)
     }
 }
 
 private fun bowlingScore(input: String): Int {
-    val throws = input.split(" ")
-        .flatMap { frame ->
-            val throw1 = frame.substring(0, 1).parseThrow()
-            val throw2 = frame.substring(1, 2).parseThrow()
-            listOf(throw1, throw2)
+    val frames = input.split(" ")
+        .map { frame ->
+            val throw1 = frame.substring(0, 1).parse9PinThrow()
+            val throw2 = frame.substring(1, 2).parseSecondThrow(throw1)
+            Frame(throw1, throw2)
         }
     var total = 0
-    for (i in throws.indices) {
-        val current = throws[i]
-        total += if (current != 10) current else current + throws[i + 1]
+    for (i in frames.indices) {
+        val current = frames[i].score()
+        total += if (current < 10) current else current + frames[i + 1].throw1
     }
     return total
 }
 
-private fun String.parseThrow() = when (this) {
-    "/" -> 10
+private fun String.parse9PinThrow() = when (this) {
     "-" -> 0
     else -> toInt()
+}
+
+private fun String.parseSecondThrow(throw1: Int) = when (this) {
+    "/" -> 10 - throw1
+    else -> parse9PinThrow()
+}
+
+data class Frame(val throw1: Int, val throw2: Int) {
+    fun score() = throw1 + throw2
 }
